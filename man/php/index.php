bc8cd48f5761960ccb067613e816b985cac340fc
manprince <manprincegod@gmail.com>
2024-01-19 16:00:21 +0700

    Changes to be committed: 	new file:   man/php/Dockerfile 	new file:   man/php/index.php 	deleted:    man/test.php

---
+++ man/php/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>PHP test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>';?>
+        <?php echo '<p>Testtttt</p>';?>
+    </body>
+</html>
